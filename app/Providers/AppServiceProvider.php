ee273476663caa7229349d6e3bd3e669ac0810d8
wahyu nurdin <nurdinwahyu2004@gmail.com>
2026-06-14 02:38:53 +0800

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+     Vite::prefetch(concurrency: 3);
+           if (env('APP_ENV') === 'local') {
+            URL::forceScheme('https');
+        }else if(env('APP_ENV') === 'production'){
+            URL::forceScheme('https');
+        }
     }
 }
